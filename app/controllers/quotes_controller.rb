b6cc4a4a12a5d6a3d86e83b2f523ef720a46f1df
Betty Mutai <bettymutai00@gmail.com>
2017-11-24 14:03:50 +0300

    Used render method in update to pass in a status code and json msg

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,12 +11,16 @@ class QuotesController < ApplicationController
 
   def create
     @quote = Quote.create!(quote_params)
-    json_response(@quote)
+    json_response(@quote, :created)
   end
 
   def update
     @quote = Quote.find(params[:id])
-    @quote.update(quote_params)
+    if @quote.update!(quote_params)
+      render status: 200, json: {
+        message: "Your quote has been updated successfully"
+      }
+    end
   end
 
   def destroy
